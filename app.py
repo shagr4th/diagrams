2381da73d8f0dd64c636ae8857bf74be5837541f
Arnaud Brochard <shagr4th@gmail.com>
2022-07-20 16:30:06 +0200

    switch to monaco editor

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+import datetime
+import subprocess
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def builder():
-    """One page app."""
-    import os
-    import datetime
-    import subprocess
-    from flask import request
-
-    template = 'app.html'
     values = {}
+    return render_template('app.html', **values)
+
+@app.route('/', methods=['POST'])
+def image():
     diag_folder = "static/"
     # get now to append to the image file to force the browser to refresh the file
     # when we edt the code as the file have the same name.
     now = datetime.datetime.now()
-
-    diagrams_data = request.form.get('diagrams_data')
+    diagrams_data = request.data
     if diagrams_data:
-        diagrams_data = diagrams_data.replace('\t', '    ')
+        #diagrams_data = diagrams_data.replace('\t', '    ')
         # dir may not exist
         if not os.path.exists(diag_folder):
           os.makedirs(diag_folder)
@@ -34,18 +33,12 @@ def builder():
             # get the pic to display
             _, _, filenames = next(os.walk(diag_folder))
             pic_name = filenames[0]
-            values.update({
-                "diagrams_data": diagrams_data,
-                "pic_name": '%s?%s' % (pic_name, now),
-            })
+            return '/static/%s?%s' % (pic_name, now)
         except Exception as err:
-            values.update({
-              "diagrams_data": diagrams_data,
-              "error": str(err).strip().replace('\\n', '<br>').replace('\\r', '<br>')
-            })
+            error = str(err).strip().replace('\\n', '<br>').replace('\\r', '<br>')
+            return error
 
-    return render_template(template, **values)
-
+    return ''
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
