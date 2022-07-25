ad5593e68427f93a4be46610b0fe2683bea79a90
Arnaud Brochard <shagr4th@gmail.com>
2022-07-25 13:02:21 +0200

    Add all Graphivz attrs to custom node

--- diagrams/custom/__init__.py
+++ diagrams/custom/__init__.py
@@ -4,6 +4,7 @@ Custom provides the possibility of load an image to be presented as a node.
 
 from diagrams import Node
 from urllib.request import urlopen
+from typing import Dict
 import hashlib
 
 class Custom(Node):
@@ -16,7 +17,7 @@ class Custom(Node):
     def _load_icon(self):
         return self._icon
 
-    def __init__(self, label, icon_path):
+    def __init__(self, label, icon_path, **attrs: Dict):
         if icon_path.startswith('data:image/'):
             with urlopen(icon_path) as response:
                 data = response.read()
@@ -25,4 +26,4 @@ class Custom(Node):
                     f.write(data)
                     f.close()
         self._icon = icon_path
-        super().__init__(label)
+        super().__init__(label, **attrs)
